3c7a55fd7fc4a9664239d71cc1f85c7e4ddc1e9b
Felipe Ryan <feliperyan@gmail.com>
2017-08-12 18:32:06 +1000

    prepping for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'EinsteinVision',
+  packages = ['EinsteinVision'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Wrapper for Salesforce\'s Einstein Predictive Vision API',
+  author = 'Felipe Ryan',
+  author_email = 'feliperyan@gmail.com',
+  url = 'https://github.com/feliperyan/EinsteinPredictiveVision', # use the URL to the github repo
+  download_url = 'https://github.com/feliperyan/EinsteinPredictiveVision/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['api', 'einsteinpredictivevision', 'metamind', 'salesforce'], # arbitrary keywords
+  classifiers = [],
+)
